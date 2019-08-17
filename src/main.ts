b7dcd6cf8c3a7b7cc5bae857114a8910add22afe
Chris Lennon <Chris.Lennon@dazn.com>
2019-08-17 17:54:53 +0200

    work around zip issue

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,54 @@
 import * as core from '@actions/core'
 import * as tc from '@actions/tool-cache'
+import * as os from 'os'
 import { promisify } from 'util'
 import { exec } from 'child_process'
 
 const execP = promisify(exec)
+const osPlat: string = os.platform()
 
 export async function run(): Promise<void> {
-    try {
-      let toolPath: string
-      toolPath = tc.find('aws', '*')
+  try {
+    let toolPath: string
+    toolPath = tc.find('aws', '*')
 
-      if (!toolPath) {
-        const downloadUrl = `https://s3.amazonaws.com/aws-cli/awscli-bundle.zip`
-        const downloadPath = await tc.downloadTool(downloadUrl)
-        const extPath = await tc.extractTar(downloadPath)
-        const installPath = `${extPath}/.local/lib/aws`
-        const binPath = `${installPath}/bin`
-        await execP(`${extPath}/awscli-bundle/install -i ${installPath}`)
-
-        new Promise(async (resolve, reject)=> {
-          exec(`${binPath}/aws --version`, async (err, stout, sterr) => {
-            const response = err ? stout : sterr
-            const regex = /(\d+\.)(\d+\.)(\d+)/g
-            const cliVersion = response.match(regex)
-            if (cliVersion !== null) {
-              const cachedPath = await tc.cacheDir(binPath, 'aws', cliVersion[0])
-              core.addPath(cachedPath)
-              resolve()
-            } else {
-              reject()
-            }
-          })
-        })
+    if (!toolPath) {
+      const downloadUrl = `https://s3.amazonaws.com/aws-cli/awscli-bundle.zip`
+      const downloadPath = await tc.downloadTool(downloadUrl)
+      let extPath: string
+      if (osPlat === 'darwin') {
+        extPath = await tc.extractTar(downloadPath)
+      } else if (osPlat === 'linux') {
+        // extPath = await tc.extractZip(downloadPath)
+        extPath = __dirname
+        await execP(`unzip ${downloadPath} -d ${extPath}`)
+      } else {
+        extPath = '/home/runner/work/_temp/'
       }
-    } catch (err) {
-      console.error(err)
+
+      const installPath = `${extPath}/.local/lib/aws`
+      const binPath = `${installPath}/bin`
+
+      await execP(`${extPath}/awscli-bundle/install -i ${installPath}`)
+
+      await new Promise(async (resolve, reject)=> {
+        exec(`${binPath}/aws --version`, async (err, stout, sterr) => {
+          const response = err ? stout : sterr
+          const regex = /(\d+\.)(\d+\.)(\d+)/g
+          const cliVersion = response.match(regex)
+          if (cliVersion !== null) {
+            const cachedPath = await tc.cacheDir(binPath, 'aws', cliVersion[0])
+            core.addPath(cachedPath)
+            resolve()
+          } else {
+            reject()
+          }
+        })
+      })
     }
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 run()
