bc044e45a8fee66ab3116c14753fc10e7a08e3a9
Chris Lennon <Chris.Lennon@dazn.com>
2019-08-23 10:04:33 +0200

    add registry patch for windows

--- src/toolHandler.ts
+++ src/toolHandler.ts
@@ -80,8 +80,8 @@ export class DownloadExtractInstall {
   }
 
   private async _getVersion(): Promise<string> {
-    const cmd: string = IS_WINDOWS ? `${this.virtualEnvFile} && ${this.installedBinaryFile}` : this.installedBinaryFile
-    const versionCommandOutput = await this._getCommandOutput(cmd, ['--version'])
+    //const cmd: string = IS_WINDOWS ? `${this.virtualEnvFile} && ${this.installedBinaryFile}` : this.installedBinaryFile
+    const versionCommandOutput = await this._getCommandOutput(this.installedBinaryFile, ['--version'])
     this.installedVersion = _filterVersion(versionCommandOutput)
     return this.installedVersion
   }
@@ -110,6 +110,12 @@ export class DownloadExtractInstall {
     const pythonPath: string = await which('python', true)
     const installCommand: string = IS_WINDOWS ? pythonPath : `${this.setupBinary} -i ${this.installDestinationDir}`
     const installArgs: string[] = IS_WINDOWS ? [this.setupBinary, '-i', this.installDestinationDir] : []
+    if (IS_WINDOWS) {
+      // We need to patch the registry to enable the virtualenv of python for the runner
+      // in hindsight it may be better to just use the MSI installer TODO
+      await exec(`echo ${this.virtualEnvFile} > "%USERPROFILE%\\.profile.cmd"`)
+      await exec('reg add "HKCU\\Software\\Microsoft\\Command Processor" /v AutoRun /t REG_SZ /d "%USERPROFILE%\\.profile.cmd" /f')
+    }
     return await exec(installCommand, installArgs)
   }
 
