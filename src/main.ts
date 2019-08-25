3daa07db798868b12940b0e3a1bfded4b2751b08
Chris Lennon <chrislennon@users.noreply.github.com>
2019-08-25 11:35:57 +0200

    remove isInstalled guard

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,21 @@ import * as path from 'path'
 const IS_WINDOWS: boolean = process.platform === 'win32' ? true : false
 
 export async function _installTool(): Promise<string>{
-  const toolPath: string = find('aws', '*')
-  if (toolPath) return toolPath
-  const awsPath: string = await which('aws', true)
-  console.log(awsPath)
-  // if (awsPath) return awsPath
-
+  
   const downloadUrl = IS_WINDOWS ? 'https://s3.amazonaws.com/aws-cli/AWSCLISetup.exe' : 'https://s3.amazonaws.com/aws-cli/awscli-bundle.zip'
   const tool = new DownloadExtractInstall(downloadUrl)
+
+  const toolPath: string = find('aws', '*')
+  if (toolPath) return toolPath
+
+  // const awsPath: string = await which('aws', true)
+  // if (awsPath) {
+  //   const toolCachePath = await tool.cacheTool(awsPath, path.join(path.parse(awsPath).dir, 'log.txt'))
+  //   console.log(awsPath)
+  //   return toolCachePath
+  // }
+  
+
   let filePath = await tool.downloadFile()
 
   if (path.parse(filePath).ext === '.zip') {
@@ -24,7 +31,7 @@ export async function _installTool(): Promise<string>{
 
   const installDestinationDir = IS_WINDOWS ? 'C:\\PROGRA~1\\Amazon\\AWSCLI' : path.join(path.parse(filePath).dir, '.local', 'lib', 'aws')
   const installArgs: string[] = IS_WINDOWS ? ['/install', '/quiet', '/norestart'] : ['-i', installDestinationDir]
-  //await tool.installPackage(filePath, installArgs)
+  await tool.installPackage(filePath, installArgs)
 
   const binFile = IS_WINDOWS ? 'aws.exe' : 'aws'
   const installedBinary = path.join(installDestinationDir, 'bin', binFile)
